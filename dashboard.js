65f3f0d17bc900f80d786fa12c55614eaa10c8a8
Tammo Tammisto <Tammo.Tammisto@tptlive.ee>
2024-10-04 11:57:25 +0300

    kristi, miks sa nii raske disaini tegid?? :(

--- dashboard.js
+++ dashboard.js
@@ -1,3 +1,25 @@
+var selectSubject = document.getElementById("selectSubject");
+var hinnang = document.getElementById("hinnang");
+const dashboardDiv = document.getElementById('dashboardData');
+
+function createDivs(subject) {
+    dashboardData.innerHTML = '';
+    const subjectBlock = document.createElement('div');
+    hinnang.innerHTML = `
+            <h2>${subject.avg_rating}*</h2>
+        `;
+
+    subject.comments.forEach(comment => {
+        const commentBlock = document.createElement('div');
+        commentBlock.innerHTML = `
+                <p><strong>${comment.student_group}</strong> ${comment.comment}</p>
+            `;
+        subjectBlock.appendChild(commentBlock);
+    });
+
+    dashboardDiv.appendChild(subjectBlock);
+}
+
 fetch('http://localhost:3000/dashboard_data')
     .then(response => response.json())
     .then(data => {
@@ -14,32 +36,59 @@ fetch('http://localhost:3000/dashboard_data')
                     };
                 })
             };
-
             subjectDataArray.push(subjectObj);
         });
         console.log(subjectDataArray);
+        // muuda tähe täidetuvust protsentides
+        //setRatingWidth(subjectDataArray[1].avg_rating * 20);
+        var subjectOrder = []
+        for (var i = 0; i < subjectDataArray.length; i++) {
+            var opt = subjectDataArray[i].subject;
+            subjectOrder.push(opt);
+            var el = document.createElement("option");
+            el.textContent = opt;
+            el.value = opt;
+            selectSubject.appendChild(el);
+        }
+        console.log(subjectOrder);
 
-        setRatingWidth(subjectDataArray[1].avg_rating * 20);
-
-        const dashboardDiv = document.getElementById('dashboardData');
-        subjectDataArray.forEach(subject => {
-            const subjectBlock = document.createElement('div');
-            subjectBlock.innerHTML = `
-                    <h2>${subject.subject} (Average Rating: ${subject.avg_rating})</h2>
-                `;
-
-            subject.comments.forEach(comment => {
-                const commentBlock = document.createElement('div');
-                commentBlock.innerHTML = `
-                        <p><strong>Group:</strong> ${comment.student_group}</p>
-                        <p><strong>Comment:</strong> ${comment.comment}</p>
-                    `;
-                subjectBlock.appendChild(commentBlock);
-            });
-
-            dashboardDiv.appendChild(subjectBlock);
-        });
-
+        var mathIndex = subjectOrder.findIndex(x => x == "Matemaatika");
+        var englishIndex = subjectOrder.findIndex(x => x == "Inglise keel");
+        var estoniaIndex = subjectOrder.findIndex(x => x == "Eesti keel");
+        var russianIndex = subjectOrder.findIndex(x => x == "Vene keel");
+        var programmingIndex = subjectOrder.findIndex(x => x == "Programmeerimine");
+        
+        document.getElementById("selectSubject").onchange = function () {
+            if (selectSubject.value == "Aine nimi") {
+                setRatingWidth(0);
+                dashboardData.innerHTML = '';
+            }
+            else if (selectSubject.value == "Matemaatika") {
+                console.log("Matemaatika");
+                setRatingWidth(subjectDataArray[mathIndex].avg_rating * 20);
+                createDivs(subjectDataArray[mathIndex]);
+            }
+            else if (selectSubject.value == "Inglise keel") {
+                console.log("Inglise keel");
+                setRatingWidth(subjectDataArray[englishIndex].avg_rating * 20);
+                createDivs(subjectDataArray[englishIndex]);
+            }
+            else if (selectSubject.value == "Eesti keel") {
+                console.log("Eesti keel");
+                setRatingWidth(subjectDataArray[estoniaIndex].avg_rating * 20);
+                createDivs(subjectDataArray[estoniaIndex]);
+            }
+            else if (selectSubject.value == "Vene keel") {
+                console.log("Vene keel");
+                setRatingWidth(subjectDataArray[russianIndex].avg_rating * 20);
+                createDivs(subjectDataArray[russianIndex]);
+            }
+            else if (selectSubject.value == "Programmeerimine") {
+                console.log("Programmeerimine");
+                setRatingWidth(subjectDataArray[programmingIndex].avg_rating * 20);
+                createDivs(subjectDataArray[programmingIndex]);
+            }
+        };
     })
     .catch(error => {
         console.error('Error fetching data:', error);
